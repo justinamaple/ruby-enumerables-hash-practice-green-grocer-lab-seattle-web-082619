72c7e8524a0dfd4e704ec899f6b19b609d0dfdc7
Justin Maple <justinamaple@gmail.com>
2019-08-21 19:21:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -7,8 +7,8 @@ def consolidate_cart(cart)
       if !cart[key][:count]
         cart[key][:count] = 1
       else
-        cart.delete(item)
         cart[key][:count] += 1
+        cart.delete(item)
       end
     end
   end
