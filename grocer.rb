ed164cfeb111796a3390475b0484e58e3777c15d
Justin Maple <justinamaple@gmail.com>
2019-08-21 19:38:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -71,7 +71,7 @@ def checkout(cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   
   p final_cart
-  total_price = final_cart.sum do |key|
+  total_price = final_cart.sum do |key, value|
     p key
     final_cart[key][:price] * final_cart[key][:count]
   end
